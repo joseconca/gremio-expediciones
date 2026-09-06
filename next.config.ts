bfc51638377fbbf8c846cd644fd749c2ec800892
Jose Conca <vl.conca@gmail.com>
2026-09-06 11:42:51 +0100

    quitar mando dev de web

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.10", "81.61.106.15"],
+  devIndicators: false
 };
 
 export default nextConfig;
