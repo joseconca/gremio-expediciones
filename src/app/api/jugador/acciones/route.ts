78e49e09ef50b000fa171b94c5dd4f77326d9a3d
Jose Conca <vl.conca@gmail.com>
2026-09-17 22:49:23 +0100

    bajar regeneracion de vida

--- src/app/api/jugador/acciones/route.ts
+++ src/app/api/jugador/acciones/route.ts
@@ -122,7 +122,7 @@ export async function POST(request: Request) {
                   velocidadMejoras: 0,
                   capacidadCarruajeMejoras: 0,
 
-                  regeneracionDeVida: 1,
+                  regeneracionDeVida: 0.1,
 
                   nivel: 1,
                   experiencia: 0,
